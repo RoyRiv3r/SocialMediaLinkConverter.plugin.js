e8a55276a0635d8cc3e0e4f09c62072654c039ab
RoyRiver <41067116+RoyRiv3r@users.noreply.github.com>
2023-11-09 02:08:36 +0100

    init commit

---
+++ TwitterX.plugin.js
@@ -0,0 +1,42 @@
+/**
+ * @name TwitterX
+ * @author Nears
+ * @description Converts specific links in user messages.
+ * @version 0.0.1
+ */
+//META{"name":"LinkConverterPlugin"}*//
+
+// Define constants for the URLs
+const ORIGINAL_URLS = /https:\/\/twitter\.com\/|https:\/\/x\.com\//g;
+const NEW_URL = "https://fxtwitter.com/";
+
+module.exports = class ExamplePlugin {
+  load() {} // Called when the plugin is loaded
+
+  start() {
+    // Called when the plugin is started
+    this.patchSendMessage();
+  }
+
+  stop() {} // Called when the plugin is stopped
+
+  patchSendMessage() {
+    const MessageActions = BdApi.findModuleByProps("sendMessage");
+    const originalFunction = MessageActions.sendMessage;
+
+    MessageActions.sendMessage = async function (channelId, messageData) {
+      try {
+        // Replace all occurrences of the original URLs with the new URL
+        messageData.content = messageData.content.replace(
+          ORIGINAL_URLS,
+          NEW_URL
+        );
+
+        // Call the original sendMessage function
+        return originalFunction.apply(this, arguments);
+      } catch (error) {
+        console.error("Error in sendMessage:", error);
+      }
+    };
+  }
+};
