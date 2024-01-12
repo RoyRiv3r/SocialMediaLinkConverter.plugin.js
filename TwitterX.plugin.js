3f75ba58e3eaf8fcf78884019b0d3f98f5e6f29d
RoyRiver <41067116+RoyRiv3r@users.noreply.github.com>
2024-01-12 01:15:11 +0100

    Added support whenever there is an attachment file and msg with url

--- TwitterX.plugin.js
+++ TwitterX.plugin.js
@@ -1,44 +1,64 @@
 /**
  * @name TwitterX
  * @author Nears
- * @description Changes Twitter links to FXTwitter format for proper embedding when shared on Discord. 
+ * @description Changes Twitter links to FXTwitter format for proper embedding when shared on Discord.
  * @donate https://ko-fi.com/royriver
  * @source https://github.com/RoyRiv3r/TwitterX.plugin.js
- * @version 0.0.1
+ * @updateURL https://raw.githubusercontent.com/RoyRiv3r/TwitterX.plugin.js/main/TwitterX.plugin.js
+ * @version 0.0.3
  */
-//META{"name":"LinkConverterPlugin"}*//
 
-// Define constants for the URLs
+//META{"name":"TwitterLinkConverter"}*//
+
+// Constants for the URLs
 const ORIGINAL_URLS = /https:\/\/twitter\.com\/|https:\/\/x\.com\//g;
 const NEW_URL = "https://fxtwitter.com/";
 
-module.exports = class ExamplePlugin {
-  load() {} // Called when the plugin is loaded
+class TwitterLinkConverter {
+  // Called when the plugin is loaded
+  load() {}
 
+  // Called when the plugin is started
   start() {
-    // Called when the plugin is started
     this.patchSendMessage();
+    this.patchUploadFiles();
   }
 
-  stop() {} // Called when the plugin is stopped
+  // Called when the plugin is stopped
+  stop() {}
 
+  // Patch the sendMessage method
   patchSendMessage() {
     const MessageActions = BdApi.findModuleByProps("sendMessage");
-    const originalFunction = MessageActions.sendMessage;
 
-    MessageActions.sendMessage = async function (channelId, messageData) {
-      try {
-        // Replace all occurrences of the original URLs with the new URL
-        messageData.content = messageData.content.replace(
-          ORIGINAL_URLS,
-          NEW_URL
-        );
-
-        // Call the original sendMessage function
-        return originalFunction.apply(this, arguments);
-      } catch (error) {
-        console.error("Error in sendMessage:", error);
+    BdApi.Patcher.before(
+      "TwitterLinkConverter",
+      MessageActions,
+      "sendMessage",
+      (_, props) => {
+        // Replace all occurrences of the original URLs with the new URL in sendMessage
+        props[1].content = props[1].content.replace(ORIGINAL_URLS, NEW_URL);
       }
-    };
+    );
   }
-};
+
+  // Patch the uploadFiles method
+  patchUploadFiles() {
+    const MessageActions = BdApi.findModuleByProps("uploadFiles");
+
+    BdApi.Patcher.before(
+      "TwitterLinkConverter",
+      MessageActions,
+      "uploadFiles",
+      (_, props) => {
+        // Check if the parsedMessage property exists before trying to replace the URL in uploadFiles
+        if (props[0].parsedMessage && props[0].parsedMessage.content) {
+          props[0].parsedMessage.content =
+            props[0].parsedMessage.content.replace(ORIGINAL_URLS, NEW_URL);
+        }
+      }
+    );
+  }
+}
+
+module.exports = TwitterLinkConverter;
